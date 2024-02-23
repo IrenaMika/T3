bc24a0a6d3d1f7f4aee17661e6fbb1fd03f75ad9
IrenaMika <ira.mika@bk.ru>
2024-02-23 22:39:56 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        int sumCredit = 1_000_000; //Сумма кредита
+        int month = 12; //Количество месяцев до погашения кредита
+        double percentCredit = 9.99; //Процент кредита
+
+        double monthPayment = (int) (service.calculate(sumCredit, month, percentCredit));
+
+        System.out.println("Ежемесячный платеж = " + Math.round(monthPayment) + " руб.");
+        System.out.println("Процентная ставка = " + percentCredit + " %");
+    }
+}
